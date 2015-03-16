3b37fc5f83e20cb47357d1c381db08685fe2c888
msrocka <michael.srocka@gmail.com>
2015-03-16 15:11:42 +0100

    jsonld: write processes with reference product

--- iodb/jsonld.py
+++ iodb/jsonld.py
@@ -4,58 +4,53 @@ import uuid
 import zipfile as azip
 
 
-class Ref:
-    def __init__(self, schema_type, uuid, name=None):
-        self.schema_type = schema_type
-        self.uuid = uuid
-        self.name = name
-
-    def as_json(self):
-        d = {"@type": self.schema_type, "@id": self.uuid}
-        if self.name is not None:
-            d["name"] = self.name
-        return d
-
-
-class RootEntity:
-    def __init__(self):
-        self.uuid = None
-        self.name = None
-        self.description = None
-
-    def as_json(self):
-        d = {
-            "@context": "http://greendelta.github.io/olca-schema/context.jsonld",
-            "@type": self.__class__.__name__,
-            "@id": self.uuid
-        }
-        if self.name is not None:
-            d["name"] = self.name
-        if self.description is not None:
-            d["description"] = self.description
-        return d
-
-
-class Process(RootEntity):
-    def __init__(self):
-        super(Process, self).__init__()
-        self.location = None
-
-    def as_json(self):
-        d = super(Process, self).as_json()
-        if self.location is not None:
-            d["location"] = self.location.as_json()
-        return d
-
-
-def make_package(tech_csv, products_csv, sat_csv):
-    pass
-
-
-def _write_product_data(product_csv, pack):
+def make_package(tech_csv, product_csv, zip_path):
+    pack = azip.ZipFile(zip_path, mode='w', compression=azip.ZIP_DEFLATED)
     _write_categories(product_csv, pack)
     _write_economic_units(pack)
     _write_products(product_csv, pack)
+    with open(product_csv, mode='r', encoding='utf-8', newline='\n') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            uid = str(uuid.uuid3(uuid.NAMESPACE_OID, "Process/" + row[0]))
+            p = init_process(row)
+            path = "processes/%s.json" % uid
+            print("write process %s" % path)
+            pack.writestr(path, json.dumps(p))
+
+
+def init_process(row):
+    uid = str(uuid.uuid3(uuid.NAMESPACE_OID, "Process/" + row[0]))
+    cat_id = get_category_id("PROCESS", row[4], row[3])
+    flow_id = str(uuid.uuid3(uuid.NAMESPACE_OID, "Flow/" + row[0]))
+    p = {
+        "@context": "http://greendelta.github.io/olca-schema/context.jsonld",
+        "@type": "Process",
+        "@id": uid,
+        "name": row[2],
+        "processTyp": "UNIT_PROCESS",
+        "category": {"@type": "Category", "@id": cat_id},
+        "processDocumentation": {"copyright": False},
+        "exchanges": [
+            {
+                "@type": "Exchange",
+                "avoidedProduct": False,
+                "input": False,
+                "amount": 1.0,
+                "flow": {"@type": "Flow", "@id": flow_id},
+                "unit": {
+                    "@type": "Unit",
+                    "@id": "3f90ee51-c78b-4b15-a693-e7f320c1e894"
+                },
+                "flowProperty": {
+                    "@type": "FlowProperty",
+                    "@id": "b0682037-e878-4be4-a63a-a7a81053a691"
+                },
+                "quantitativeReference": True
+            }
+        ]
+    }
+    return p
 
 
 def _write_products(product_csv, pack):
@@ -164,23 +159,8 @@ def _write_economic_units(pack):
                   fp)
 
 
-class SectorInfo:
-    def __init__(self):
-        self.name = None
-        self.product_id = None
-        self.process_id = None
+if __name__ == '__main__':
+    make_package("../build/dr_coefficients.csv",
+                 "../build/products.csv", '../build/package.zip')
 
-
-    if __name__ == '__main__':
-        pack = azip.ZipFile('../build/package.zip', mode='w',
-                            compression=azip.ZIP_DEFLATED)
-        # _write_product_data('../build/products.csv', pack)
-        _write_product_data('../build/products.csv', pack)
-
-        """
-        p = Process()
-        p.name = "Test"
-        p.location = Ref("Location", "uuid")
-        print(json.dumps(p.as_json()))
-        """
 
