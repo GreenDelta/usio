a743837cf58c22f382cbd4ae92414f3ee966e1fa
msrocka <michael.srocka@gmail.com>
2015-03-26 12:30:58 +0100

    added product extraction  to pipeline

--- make.py
+++ make.py
@@ -5,13 +5,18 @@ if __name__ == '__main__':
     # input data sources
     bea_make = 'data/bea2002/redef/IOMakeDetail.txt'
     bea_use = 'data/bea2002/redef/IOUseDetail.txt'
+    categories = 'data/naics_categories.json'
 
     # (intermediate) outputs
     out_dir = 'build/pipeline/'
     make = out_dir + 'make.csv'
     use = out_dir + 'use.csv'
+    tech = out_dir + 'tech.csv'
+    products = out_dir + 'products.csv'
 
     pipe.execute(
-        pipe.Bea2002MakeTransformation(bea_make, make),
-        pipe.Bea2002UseTransformation(bea_use, use)
+        pipe.Bea2002MakeTransformation.on(bea_make).to(make),
+        pipe.Bea2002UseTransformation(bea_use, use),
+        pipe.TechMatrixTransformation(make, use, tech),
+        pipe.ProductExtractor(tech, categories, products)
     )
--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,4 +1,6 @@
 import pipeline.bea2002 as bea
+import pipeline.dr as dr
+import pipeline.products as products
 
 
 def execute(*transformations):
@@ -13,6 +15,14 @@ class Bea2002MakeTransformation:
         self.bea_make = bea_make_file
         self.csv_make = csv_output_file
         pass
+
+    @classmethod
+    def on(cls, bea_makefile):
+        return Bea2002MakeTransformation(bea_makefile, None)
+
+    def to(self, csv_output_file):
+        self.csv_make = csv_output_file
+        return self
 
     def description(self):
         return "convert BEA 2002 make matrix %s to CSV matrix %s" % (
@@ -34,3 +44,36 @@ class Bea2002UseTransformation:
 
     def run(self):
         bea.use_to_csv(self.bea_use, self.csv_use)
+
+
+class TechMatrixTransformation:
+
+    def __init__(self, make_csv_file, use_csv_file, dr_csv_file):
+        self.make_csv = make_csv_file
+        self.use_csv = use_csv_file
+        self.dr_csv = dr_csv_file
+
+    def description(self):
+        return "calculate the DR coefficient matrix %s from %s and %s" % (
+            self.dr_csv, self.make_csv, self.use_csv)
+
+    def run(self):
+        make_table = dr.read_sparse_matrix(self.make_csv)
+        use_table = dr.read_sparse_matrix(self.use_csv)
+        dr_matrix = dr.calculate_dr_coefficients(use_table, make_table)
+        dr_matrix.write_sparse_csv(self.dr_csv)
+
+
+class ProductExtractor:
+
+    def __init__(self, tech_csv_file, category_json_file, out_csv_file):
+        self.tech_csv = tech_csv_file
+        self.cat_json = category_json_file
+        self.out_csv = out_csv_file
+
+    def description(self):
+        return "extract product/sector information from %s to %s" % (
+            self.tech_csv, self.out_csv)
+
+    def run(self):
+        products.make_product_table(self.tech_csv, self.cat_json, self.out_csv)
